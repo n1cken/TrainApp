74320167515f9f65937933e64b034c1b269a5ad8
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-12-22 23:27:08 +0100

    Note

--- backend/routes/trafikverket.js
+++ backend/routes/trafikverket.js
@@ -7,6 +7,20 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('Hello World!')
 
+  /**
+   * <REQUEST>
+  <LOGIN authenticationkey="{	320718ba25b94e40b84c6e74d51f8bf8}" />
+  <QUERY objecttype="TrainMessage" schemaversion="1.3">
+    <FILTER>
+      <EQ name="AffectedLocation" value="Cst" />
+    </FILTER>
+    <INCLUDE>StartDateTime</INCLUDE>
+    <INCLUDE>LastUpdateDateTime</INCLUDE>
+    <INCLUDE>ExternalDescription</INCLUDE>
+    <INCLUDE>ReasonCodeText</INCLUDE>
+  </QUERY>
+</REQUEST>
+   */
   // fetch data from trafikverket
 
   //insert data into local array
