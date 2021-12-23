d9d515d4d887baa49105e92c9007ee29f544d439
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-12-23 10:47:24 +0100

    Has made some refinement of the response but better-sqlite is hard to work with

--- backend/routes/trafikverket.js
+++ backend/routes/trafikverket.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const fetch = require('node-fetch')
 
+const settings = require('../settings.json');
+const path = require('path');
+
 const router = express.Router();
 
-//connect to db
+
+const driver = require('better-sqlite3');
+const { appendFile } = require('fs');
+const db = driver(path.join('../',
+      'database', settings.dbName));
 
 router.get('/', async (req, res) => {
-  const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json',{
-    method:'POST',
-    body:`<REQUEST>
+      const response = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
+            method: 'POST',
+            body: `<REQUEST>
     <LOGIN authenticationkey="320718ba25b94e40b84c6e74d51f8bf8" />
     <QUERY objecttype="TrainAnnouncement" schemaversion="1.3" orderby="AdvertisedTimeAtLocation">
           <FILTER>
@@ -27,20 +34,28 @@ router.get('/', async (req, res) => {
           <INCLUDE>AdvertisedTrainIdent</INCLUDE>
           <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
           <INCLUDE>ToLocation.LocationName</INCLUDE>
+          <INCLUDE>LocationSignature</INCLUDE>
     </QUERY>
 </REQUEST>`
-  });
-  const body = await response.json();
-  res.send(body.RESPONSE.RESULT[0].TrainAnnouncement[2]);
-  // fetch data from trafikverket
+      });
+      const body = await response.json();
+      const TrainAnnouncement = body.RESPONSE.RESULT[0].TrainAnnouncement
+      res.send(body.RESPONSE.RESULT[0].TrainAnnouncement);
+      // // fetch data from trafikverket
+      // // prepare insert statement, only push data we want 
 
+      //!!!!!! better sqlite is being difficult to work with
+      // for (let i = 0; i < TrainAnnouncement.length; i++) {
+      //       console.log(TrainAnnouncement[i].AdvertisedTimeAtLocation)
+      //       let addStation = db.prepare(`
+      //       INSERT INTO trainDepartures( origin, destination, date, train_index)
+      //       VALUES (3,1, ${TrainAnnouncement[i].AdvertisedTimeAtLocation}
+      //             ,${TrainAnnouncement[i].AdvertisedTrainIdent})
+      //       `);
+      //       addStation.all();
+      // };
+      //push it into db
 
-
-  //insert data into local array
-
-  // prepare insert statement, only push data we want 
-
-  //push it into db
 });
 
 module.exports = router;
