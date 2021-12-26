068142cbf37c0eaa5c928ea5738e4a1a38cf362c
Leo Rönnebro <{{ .email }}>
2021-12-26 22:10:31 +0100

    Fixed cors

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,18 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-console.log(process.env.PORT)
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 import routes from './routes/index.js';
 
 // Middleware
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/station', routes.station)
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
