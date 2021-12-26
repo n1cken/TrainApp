f926e9bce5771b47c58b32160fb319c0a82a53e6
Leo Rönnebro <{{ .email }}>
2021-12-26 22:10:42 +0100

    Added example of fetching by url parameter

--- backend/routes/station.js
+++ backend/routes/station.js
@@ -11,7 +11,14 @@ const db = driver(path.join( '../', 'database', process.env.DBNAME));
 
 router.get('/', (req, res) => {
   let stations = db.prepare(`SELECT * FROM station`).all();
-  res.send(stations)
+  res.send(stations);
+});
+
+// TODO: Error handling
+router.get('/:id', (req, res) => {
+  let stmt = db.prepare(`SELECT * FROM station WHERE id=?`);
+  var station = stmt.get(req.params.id);
+  res.send(station);
 });
 
 export default router;
