84e6af75e21c110e7285fad651a11f811317f5ac
Leo Rönnebro <{{ .email }}>
2021-12-28 22:01:14 +0100

    Dynamic imports of handlers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,14 @@
-const station = require('./station.js');
+const fs = require('fs');
+const path = require('path');
 
 function InitRoutes(app, db) {
-  app.use('/station', station(db))
+  fs.readdirSync(__dirname)
+    .filter(file => {
+      return (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js')
+    }).forEach(file => {
+      const handler = require(`${path.join(__dirname, file)}`)(db)
+      app.use(`/${file.replace(/\.[^/.]+$/, "")}`, handler)
+    })
 }
 
 module.exports = {
