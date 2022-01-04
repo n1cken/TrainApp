4159926b7f1b797bc0288a7cbfac634a3e33a9ac
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2022-01-04 10:18:23 +0100

    Update timetable route

--- backend/routes/timetable.js
+++ backend/routes/timetable.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 // TODO: Error handling
 module.exports = (db) => {
   const router = express.Router();
@@ -8,12 +9,20 @@ module.exports = (db) => {
     res.send('working')
   })
 
-  router.get('/:origin/:destination', async (req, res) => {
+  router.get('/:origin/:destination/', async (req, res) => {
+
+    //translate stations into db corresponding id
     const UserOrigin = decodeURIComponent(req.params.origin);
     const UserDestination = decodeURIComponent(req.params.destination);
     const OG = await db.station.findOne({ where: { name: UserOrigin } });
     const DN = await db.station.findOne({ where: { name: UserDestination } });
-    res.json(DN.id)
+
+    const result = await db.timetable.findAll({
+      // find all instance of destination filter and for those who match with the route that has origin before destination, 
+      //lastly filter with datetime
+    })
+    res.send(result)
+    // use id to find in timetable
   });
 
   return router;
