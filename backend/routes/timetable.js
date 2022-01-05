4f488efa68036312d2c8d0f0f38150e9dab9a7ca
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2022-01-05 11:57:32 +0100

    Filter result from db

--- backend/routes/timetable.js
+++ backend/routes/timetable.js
@@ -9,7 +9,7 @@ module.exports = (db) => {
   })
 
   router.get('/:origin/:destination/:date', async (req, res) => {
-
+    const FinalizedTimetable = []
     //translate stations into db corresponding id
     const UserOrigin = decodeURIComponent(req.params.origin);
     const UserDestination = decodeURIComponent(req.params.destination);
@@ -17,7 +17,29 @@ module.exports = (db) => {
     const DN = await db.station.findOne({ where: { name: UserDestination } });
 
     const result = await db.timetable.findAll({ where: { [Op.or]: [{ stationId: OG.id }, { stationId: DN.id }], [Op.and]: [{ departure: { [Op.lte]: `${req.params.date}T23:59:59Z` } }, { departure: { [Op.gte]: `${req.params.date}T00:00:01Z` } }] } })
-    res.send(result)
+
+    for (let i = 0; i < result.length; i++) {
+      const localArrayMatch = FinalizedTimetable.filter(singleElement => singleElement.routeId == result[i].routeId)
+      if (localArrayMatch.length < 1) {
+        const match = result.filter(timetable => timetable.routeId == result[i].routeId)
+        if (match[0].routeId == DN) {
+          FinalizedTimetable.push({ "departure": match[1].departure, "arrival": match[0].arrival, "From": UserOrigin, "To": UserDestination, "routeId": result[i].routeId })
+        } else {
+          FinalizedTimetable.push({ "departure": match[0].departure, "arrival": match[1].arrival, "From": UserOrigin, "To": UserDestination, "routeId": result[i].routeId })
+        }
+      } else {
+      }
+    }
+
+    console.log(FinalizedTimetable)
+    // take one single result routeId
+    // check if element exists in Local Array
+    // if Not ///////////////////////////
+    // find other pair in result
+    //if yes ////////////////////////////
+    //Break and move on
+
+    res.send(FinalizedTimetable)
     // use id to find in timetable
   });
 
