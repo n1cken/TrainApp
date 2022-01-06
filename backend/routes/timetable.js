2ffb972125db9de8893d83466f770e013c9e1dee
TheNerdyHamster <leo@letnh.com>
2022-01-06 22:24:53 +0100

    Update endpoint

--- backend/routes/timetable.js
+++ backend/routes/timetable.js
@@ -4,20 +4,23 @@ const { Op } = require("sequelize");
 module.exports = (db) => {
   const router = express.Router();
 
-  router.get('/', (req, res) => {
-    res.send('working')
-  })
+  router.get('/', async (req, res) => {
+    let queries = req.query
+    if (queries.from === undefined || queries.dest === undefined || queries.date === undefined)
+      return res.status(404).end("Please specify query")
 
-  router.get('/:origin/:destination/:date', async (req, res) => {
 
-    const FinalizedTimetable = []
-    //translate stations into db corresponding id
-    const UserOrigin = decodeURIComponent(req.params.origin);
-    const UserDestination = decodeURIComponent(req.params.destination);
-    const OG = await db.station.findOne({ where: { name: UserOrigin } });
-    const DN = await db.station.findOne({ where: { name: UserDestination } });
+    const OG = await db.station.findByPk(queries.from)
+    if (OG === null)
+        return res.status(404).end("From staion not found")
 
-    const result = await db.timetable.findAll({ where: { [Op.or]: [{ stationId: OG.id }, { stationId: DN.id }], [Op.and]: [{ [Op.or]: [{ departure: { [Op.lte]: `${req.params.date}T23:59:59Z` }, departure: { [Op.gte]: `${req.params.date}T00:00:01Z` } }, { arrival: { [Op.lte]: `${req.params.date}T23:59:59Z` }, arrival: { [Op.gte]: `${req.params.date}T00:00:01Z` } }] }] } })
+    const DN = await db.station.findByPk(queries.dest)
+    if (DN === null)
+        return res.status(404).end("Destination station not found")
+
+    let FinalizedTimetable = []
+
+const result = await db.timetable.findAll({ where: { [Op.or]: [{ stationId: OG.id }, { stationId: DN.id }], [Op.and]: [{ [Op.or]: [{ departure: { [Op.lte]: `${queries.date}T23:59:59Z` }, departure: { [Op.gte]: `${queries.date}T00:00:01Z` } }, { arrival: { [Op.lte]: `${queries.date}T23:59:59Z` }, arrival: { [Op.gte]: `${queries.date}T00:00:01Z` } }] }] } })
 
     for (let i = 0; i < result.length; i++) {
       const localArrayMatch = FinalizedTimetable.filter(singleElement => singleElement.routeId == result[i].routeId)
@@ -26,27 +29,17 @@ module.exports = (db) => {
         if (match.length == 2) {
           console.log(match.length)
           if (match[0].routeId == DN) {
-            FinalizedTimetable.push({ "departure": match[1].departure, "arrival": match[0].arrival, "From": UserOrigin, "To": UserDestination, "routeId": result[i].routeId })
+            FinalizedTimetable.push({ "departure": match[1].departure, "arrival": match[0].arrival, "From": OG.name, "To": DN.name, "routeId": result[i].routeId })
           } else {
-            FinalizedTimetable.push({ "departure": match[0].departure, "arrival": match[1].arrival, "From": UserOrigin, "To": UserDestination, "routeId": result[i].routeId })
+            FinalizedTimetable.push({ "departure": match[0].departure, "arrival": match[1].arrival, "From": OG.name, "To": DN.name, "routeId": result[i].routeId })
           }
         }
 
-      } else {
       }
     }
 
-    console.log(FinalizedTimetable)
-    // take one single result routeId
-    // check if element exists in Local Array
-    // if Not ///////////////////////////
-    // find other pair in result
-    //if yes ////////////////////////////
-    //Break and move on
-
     res.send(FinalizedTimetable)
-    // use id to find in timetable
-  });
+  })
 
   return router;
 }
