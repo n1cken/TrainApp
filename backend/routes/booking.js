762989f4786cb4f0e3f694c649a183120a0d3df3
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2022-01-10 14:00:20 +0100

    basic booking implementation

---
+++ backend/routes/booking.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+
+module.exports = (db) => {
+  const router = express.Router();
+
+  router.post('/', async (req, res) => {
+
+
+
+    //UID creator
+    var uniqueId = function () {
+      var letters = "123456789";
+      var Id = "";
+      for (var i = 0; i < 5; i++) {
+        Id += letters.charAt(Math.floor(Math.random() * letters.length));
+      }
+      return Id;
+    };
+
+    //Query
+    const { routeId, timetableArrivalId, timetableDepartureId } = req.body
+    try {
+      const createBooking = await db.booking.create({
+        id: uniqueId(),
+        timetableArrivalId,
+        email,
+        timetableDepartureId
+      });
+      return res.json(createBooking)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json(err)
+    }
+
+    res.send("Booking Created");
+  })
+
+  return router;
+}
