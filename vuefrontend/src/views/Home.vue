1f63d161674ac80268b3db2f79c1d94338f5ea66
Leo Rönnebro <leo@letnh.com>
2022-01-13 11:13:17 +0100

    Fixed G2-96 and removed unused console.logs

--- vuefrontend/src/views/Home.vue
+++ vuefrontend/src/views/Home.vue
@@ -135,7 +135,7 @@ export default {
       fetchingResult: false,
       missingStations: false,
       sameStations: false,
-      validSearch: null,
+      validSearch: true,
       results: [],
       amountOfTickets: this.$store.state.chosenAmountOfTickets,
       searchResultDepartureDate: null,
@@ -193,45 +193,46 @@ export default {
       }
 
       //Check that fields are not null.
-      if (
-        this.$store.state.originStation &&
-        this.$store.state.destinationStation &&
-        this.$store.state.chosenDepartureDate
-      ) {
-        //If same origin and destination
-        if (this.$store.state.originStation == this.$store.state.destinationStation) {
-          this.sameStations = true;
-          this.missingStations = false;
-          this.validSearch = false;
-        }
-
-        //Valid search
-        if (this.$store.state.originStation != this.$store.state.destinationStation) {
-          this.sameStations = false;
-          this.missingStations = false;
-          this.validSearch = true;
-        }
-
-        if (this.validSearch) {
-          const url = `${process.env.VUE_APP_API_URL}/route?from=${this.$store.state.originStation}&dest=${this.$store.state.destinationStation}&date=${this.$store.state.chosenDepartureDate}`;
-          fetch(url)
-            .then((res) => res.json())
-            .then((data) => (this.resultData = data))
-            .then(() => {
-              for (var i = 0; i < this.resultData.length; i++) {
-                this.results.push(this.resultData[i]);
-              }
-              console.log(this.resultData);
-              console.log("result array: ", this.results);
-              this.fetchingResult = false
-            })
-            .catch((err) => {
-              this.fetchingResult = false
-              console.log(err.message)
-            });
-        }
+      if (this.missingStations || !this.$store.state.chosenDepartureDate) {
+        this.fetchingResult = false
+        return
       }
-    },
-  },
-};
+
+      //If same origin and destination
+      if (this.$store.state.originStation == this.$store.state.destinationStation) {
+        this.sameStations = true;
+        this.fetchingResult = false
+        return
+      }
+
+      //Valid search
+      if (this.$store.state.originStation == this.$store.state.destinationStation) {
+        this.validSearch = false;
+        this.fetchingResult = false
+        return
+      }
+
+      if (!this.validSearch) {
+        this.fetchingResult = false
+        return
+      }
+
+      const url = `${process.env.VUE_APP_API_URL}/route?from=${this.$store.state.originStation}&dest=${this.$store.state.destinationStation}&date=${this.$store.state.chosenDepartureDate}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => (this.resultData = data))
+        .then(() => {
+          for (var i = 0; i < this.resultData.length; i++) {
+            this.results.push(this.resultData[i]);
+          }
+          this.fetchingResult = false
+        })
+        .catch((err) => {
+          this.fetchingResult = false
+          console.log(err.message)
+        });
+      }
+    }
+  };
 </script>
