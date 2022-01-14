1d7a8c2f799fe3b2cbbc90df7a0a2fa1d61eed6b
nicholas <nicken333@gmail.com>
2022-01-14 14:25:04 +0100

    removing ugly T and Z from date

--- vuefrontend/src/views/Home.vue
+++ vuefrontend/src/views/Home.vue
@@ -224,6 +224,12 @@ export default {
             .then((data) => (this.resultData = data))
             .then(() => {
               for (var i = 0; i < this.resultData.length; i++) {
+                this.resultData[i].arrival = this.resultData[i].arrival
+                  .replace("T", " ")
+                  .replace("Z", " ");
+                this.resultData[i].departure = this.resultData[i].departure
+                  .replace("T", " ")
+                  .replace("Z", " ");
                 this.results.push(this.resultData[i]);
               }
               setTimeout(() => {
